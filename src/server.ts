cb5b24968cc6e7b14f81c7953d9693a8f49d6b7c
Dave <davearonmwan@gmail.com>
2025-11-01 15:00:49 +0100

    chore: revert cors config test

--- src/server.ts
+++ src/server.ts
@@ -7,11 +7,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(
-	cors({
-		origin: "https://dynamic-profile-backend.onrender.com",
-	})
-);
+app.use(cors());
 
 app.get("/", (_, res) => {
 	return res.status(200).json({ data: "Server is up and running!" });
